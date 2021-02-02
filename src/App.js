e1957b5a29670aa438a9bd8af60e454da9182074
evannor <erica.vannorman@gmail.com>
2021-02-01 16:43:41 -0800

    Show current date

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 
 function App() {
+  let currentDate = new Date();
+
   return (
     <div className="App">
       <h1>Welcome to the Pomodoro Timer App</h1>
+      <h3>The current date and time:</h3>
+      <div>{currentDate.toDateString()}</div>
     </div>
   );
 }
